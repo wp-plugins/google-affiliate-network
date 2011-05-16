a6568dd4a0028e58bffd74ddc0247e2fe84cd825
RobertPHeller <RobertPHeller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-16 16:41:45 +0000

    Pre-release 4.0.1.
    git-svn-id: https://plugins.svn.wordpress.org/google-affiliate-network/tags/4.0.1@385520 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ GAN_Constants.php
@@ -0,0 +1,31 @@
+<?php
+
+/* GAN_Constants.php
+ * Assorted global constants.  These constants are used in various places in
+ * the code.
+ */
+
+define('GAN_PLUGIN_NAME', 'GAN_Plugin'); /* Name of the plugin */
+define('GAN_DIR', dirname(__FILE__));    /* The Plugin directory */
+define('GAN_VERSION', '4.0.1');		 /* The Plug in version */
+/* Plug in display name */
+define('GAN_DISPLAY_NAME', 'Google Affiliate Network Plugin');
+/* Base URL of the plug in */
+define('GAN_PLUGIN_URL', get_bloginfo('wpurl') . '/wp-content/plugins/' . basename(GAN_DIR));
+/* URL of the Plugin's CSS dir */
+define('GAN_PLUGIN_CSS_URL', GAN_PLUGIN_URL . '/css');
+/* URL of the Plugin's image dir */
+define('GAN_PLUGIN_IMAGE_URL', GAN_PLUGIN_URL . '/images');
+global $wpdb;
+/* Database table names */
+define('GAN_AD_TABLE',$wpdb->prefix . "DWS_GAN");	/* Base ad table */
+define('GAN_MERCH_TABLE',$wpdb->prefix . "DWS_GAN_MERCH");       /* Merchant Table */
+/* Old (pre V3) tables and views */
+/* Ad statistics */
+define('GAN_AD_STATS_TABLE',$wpdb->prefix . "DWS_GAN_AD_STATS");
+/* Merchant statistics */
+define('GAN_MERCH_STATS_TABLE',$wpdb->prefix . "DWS_GAN_MERCH_STATS");
+/* Views (combinations of Ad database and statistics tables) */
+define('GAN_AD_STATS_TABLE_VIEW',$wpdb->prefix . "DWS_GAN_AD_STATS_VIEW");
+define('GAN_MERCH_STATS_TABLE_VIEW',$wpdb->prefix . "DWS_GAN_MERCH_STATS_VIEW");
+?>
