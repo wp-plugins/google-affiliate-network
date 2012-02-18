ffde31cb332f487ca7d985f2f74c2ef6656151b2
RobertPHeller <RobertPHeller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-18 19:12:12 +0000

    Prepare for maintainence release
    git-svn-id: https://plugins.svn.wordpress.org/google-affiliate-network/trunk@507065 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- GAN.php
+++ GAN.php
@@ -3,7 +3,7 @@
  * Plugin Name: Google Affiliate Network widget
  * Plugin URI: http://http://www.deepsoft.com/GAN
  * Description: A Widget to display Google Affiliate Network ads
- * Version: 5.0.3
+ * Version: 5.0.4
  * Author: Robert Heller
  * Author URI: http://www.deepsoft.com/
  * License: GPL2
